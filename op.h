f465b1434865425dc0bf5fabcf3a74f11c126c6b
wiserin <timofeytsep@gmail.com>
2025-11-01 23:18:02 +0300

    init op

---
+++ op.h
@@ -0,0 +1,28 @@
+#ifndef __op__
+#define __op__
+
+#define ровное int
+#define зумерское float
+#define туда_ровный int*
+#define поясни_за printf
+#define подкрепи +
+#define ибо <
+#define дай &
+#define Практика func
+#define попытка b
+#define защита if
+#define пожалуйста *
+#define допса else if
+#define ПСЖ else
+#define результат return
+#define ОП while
+#define отчисление a
+#define да 1
+#define нет 0
+#define что %s
+#define парадокс !=
+#define следующая += 1
+#define бывает ==
+
+
+#endif
