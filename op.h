80184887cd60c860e8e1b4befbe3c2d87ac31e23
wiserin <timofeytsep@gmail.com>
2025-11-01 23:54:32 +0300

    hm

--- op.h
+++ op.h
@@ -2,27 +2,37 @@
 #define __op__
 
 #define ровное int
-#define зумерское float
 #define туда_ровный int*
+#define зумерское float
+#define туда_зумерское float*
+#define паста str
+#define туда_паста str*
+#define буква char
+#define туда_буква char*
+
 #define поясни_за printf
-#define подкрепи +
-#define ибо <
-#define дай &
-#define Практика func
-#define попытка b
 #define защита if
-#define пожалуйста *
 #define допса else if
 #define ПСЖ else
 #define результат return
 #define ОП while
-#define отчисление a
+#define зачет switch
+#define рандом case
+#define провал default
+
+#define дай &
+#define пожалуйста *
+
 #define да 1
 #define нет 0
-#define что %s
+
 #define парадокс !=
 #define следующая += 1
 #define бывает ==
+#define ибо <
 
+#define Практика func
+#define попытка b
+#define отчисление a
 
 #endif
